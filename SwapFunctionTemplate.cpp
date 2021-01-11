e40072fc6e8ae82efb74b239e7f6f83e536ac282
ryuu9505 <66634707+ryuu9505@users.noreply.github.com>
2021-01-11 15:44:03 +0900

    Update SwapFunctionTemplate.cpp

--- SwapFunctionTemplate.cpp
+++ SwapFunctionTemplate.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 
+// Swap Function Template
 template <typename T>
 void Swap(T* a, T* b)
 {
@@ -8,6 +9,26 @@ void Swap(T* a, T* b)
 	temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+// Max Function Template
+template <typenmae T>
+T Max(T a, T b)
+{
+	return a >= b ? a : b;
+}
+
+// Specialization of Max Function Template
+template <>
+char* Max(char* a, char* b) // "char* Max()는 이 함수를 사용하라"
+{
+	return strlen(a) >= strlen(b) ? a : b;
+}
+
+template <>
+const char* Max(const char* a, const char* b) // "const char* Max()는 이 함수를 사용하라"
+{
+	return strcmp(a, b) >= 0 ? a : b;
 }
 
 int main()
